ae65d59fdc07e758f1fef0b3da29d20ac4653d59
Deepak Belamana <53257796+deepakbelamana@users.noreply.github.com>
2022-06-26 23:12:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def downloadVideo():
     yt = YouTube(url) 
     mp4files = yt.streams.filter(file_extension='mp4').get_highest_resolution().itag #getting higher resolution stream itag
     d_video=yt.streams.get_by_itag(mp4files) #getting stream based on itag
-    print(d_video.default_filename) #default_filename gives the name of the video
+    print(d_video.default_filename,save_path) #default_filename gives the name of the video
     d_video.download(save_path) #downloading the video to path specified in save_path
     response=send_file(save_path+d_video.default_filename,as_attachment=True,download_name=d_video.default_filename) #sending the downloaded video 
     return response
